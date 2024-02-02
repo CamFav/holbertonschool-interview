411de0bac54a6b3176b88b4414b0859a4fffc798
Camille F <113915795+CamFav@users.noreply.github.com>
2024-02-02 22:41:46 +0100

    Create sandpiles.h

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void add_matrix(int grid1[3][3], int grid2[3][3]);
+void print_grid2(int grid[3][3]);
+void toppling(int grid1[3][3]);
+
+#endif

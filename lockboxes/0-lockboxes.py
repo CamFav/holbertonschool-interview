bf09bcac4b5e9438d2115d45f1afb55a53d6b4bd
CamilleFavriel <camillefvrl@gmail.com>
2024-05-09 22:43:39 +0200

    method that determines if all the boxes can be opened using dfs algo

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    # Visited boxes
+    visited = set()
 
+    # DFS Algo
+    def dfs(box_index):
+
+        visited.add(box_index)
+
+        for key in boxes[box_index]:
+            if key not in visited:
+                dfs(key)
+
+    # Start DFS from box 0
+    dfs(0)
+
+    return len(visited) == len(boxes)

3b10787b77221183d72af3c226beefac14359cf3
CamilleFavriel <camillefvrl@gmail.com>
2024-05-09 22:48:09 +0200

    [Fix] PEP8 Style

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
+
 def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists
+
+    Returns:
+        bool: True if all boxes can be opened; otherwise, False.
+
+    Description:
+        This function utilizes depth-first search (DFS) to explore the boxes
+        and their keys. It keeps track of visited boxes to ensure that all
+        reachable boxes are visited. It determines what 
+        boxes can be opened based on the traversal results.
+    """
     # Visited boxes
     visited = set()
 
     # DFS Algo
     def dfs(box_index):
+        """Explore boxes recursively using DFS."""
 
         visited.add(box_index)
 

f2271c180f6214579f4437491e4c33d42a1298e2
CamilleFavriel <camillefvrl@gmail.com>
2024-08-23 22:57:26 +0200

    Temporary file

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,38 +1,27 @@
 #!/usr/bin/node
-
-const axios = require('axios');
-
-// Get the Movie ID
-const movieId = process.argv[2];
-
-// Check if movieId is provided
-if (!movieId) {
-    console.error('Please provide a Movie ID.');
-    process.exit(1);
-}
-
-// Base URL for the Star Wars API
-const baseUrl = 'https://swapi.dev/api/films/';
-
-// Fetch and display characters
-async function fetchCharacters(movieId) {
-    try {
-        // Fetch movie details
-        const response = await axios.get(`${baseUrl}${movieId}/`);
-        const movieData = response.data;
-
-        // Get the list of character URLs
-        const characterUrls = movieData.characters;
-
-        // Fetch and display each character's name
-        for (const url of characterUrls) {
-            const characterResponse = await axios.get(url);
-            console.log(characterResponse.data.name);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
+const request = require('request');
+const myArgs = process.argv.splice(2);
+const URL = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+request.get(URL, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const character = JSON.parse(body).characters;
+    const characterList = characterURLs => {
+      const promise = new Promise((resolve, reject) => {
+        request.get(characterURLs, (err, response, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(body);
+          }
+        });
+      });
+      return promise;
+    };
+    for (let i = 0; i < character.length; i++) {
+      const result = await characterList(character[i]);
+      console.log(JSON.parse(result).name);
     }
-}
-
-// Call the function to fetch characters
-fetchCharacters(movieId);
+  }
+});

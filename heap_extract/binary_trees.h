46edbb38d15f18f1f2e3602b0941e5c9f5843e59
CamilleFavriel <camillefvrl@gmail.com>
2024-09-27 23:33:13 +0200

    Fixed header file

--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -1,7 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,15 +13,19 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_print(const binary_tree_t *);
-heap_t *last_node = get_last_node(root_node);
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
-#endif /* _BINARY_TREES_H_ */
+heap_t *get_last_node(heap_t *root);  // Déclarez la fonction get_last_node
+void heapify_down(heap_t *node);
+int heap_extract(heap_t **root);
+
+#endif /* BINARY_TREES_H */

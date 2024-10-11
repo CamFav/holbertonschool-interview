d3083c0c51d417a0bfed077851b21083d8d0418b
CamilleFavriel <camillefvrl@gmail.com>
2024-10-11 16:16:06 +0200

    Betty Coding Style

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -8,25 +8,24 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next; /* Move slow pointer by 1 */
-        fast = fast->next->next; /* Move fast pointer by 2 */
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next; /* Move slow pointer by 1 */
+		fast = fast->next->next; /* Move fast pointer by 2 */
 
-        if (slow == fast)
-        {
-            slow = head; /* Move slow to head */
-            while (slow != fast) /* Move both pointers at same pace */
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow); /* Loop start */
-        }
-    }
-
-    return (NULL);
+		if (slow == fast)
+		{
+			slow = head; /* Move slow to head */
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow); /* Loop start */
+		}
+	}
+	return (NULL);
 }

f6b2096e5a9ced80f9de6562b4d4e09dd4c1ca4c
CamilleFavriel <camillefvrl@gmail.com>
2024-11-01 16:05:03 +0100

    Betty coding style

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -6,44 +6,41 @@
  * @list: Pointer to the head of the list
  * @str: String to be duplicated and stored in the new node
  * 
- * Return: Adress of the new node, otherwise NULL
+ * Return: Address of the new node, otherwise NULL
  */
-List *add_node_end(List **list, char *str) {
-    List *new_node, *tail;
+List *add_node_end(List **list, char *str)
+{
+	List *new_node, *tail;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(List));
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-    if (!new_node)
-        return (NULL);
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    /* Duplicate the string */
-    new_node->str = strdup(str);
-
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    /* If list is empty, set prev and next pointers of the node to itsef. */
-    if (!*list)
-    {
-        new_node->next = new_node;
-        new_node->prev = new_node;
-        *list = new_node;
-    }
-
-    /* Else, if list is not empty, set the pointers to insert it at the end */
-    else
-    {
-        tail = (*list)->prev;
-        tail->next = new_node;
-        new_node->prev = tail;
-        new_node->next = *list;
-        (*list)->prev = new_node;
-    }
-    return (new_node);
+	/* If list is empty, set prev and next pointers of the node to itself */
+	if (!*list)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else /* List is not empty, insert at the end */
+	{
+		tail = (*list)->prev;
+		tail->next = new_node;
+		new_node->prev = tail;
+		new_node->next = *list;
+		(*list)->prev = new_node;
+	}
+	return (new_node);
 }
 
 /**
@@ -52,43 +49,40 @@ List *add_node_end(List **list, char *str) {
  * @list: Pointer to the head of the list
  * @str: String to be duplicated and stored in the new node
  * 
- * Return: Adress of the new node, otherwise NULL
+ * Return: Address of the new node, otherwise NULL
  */
-List *add_node_begin(List **list, char *str) {
-    List *new_node, *tail;
+List *add_node_begin(List **list, char *str)
+{
+	List *new_node, *tail;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(List));
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-    if (!new_node)
-        return (NULL);
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    /* Duplicate the string */
-    new_node->str = strdup(str);
-
-    if (!new_node->str)
-    {
-        free(new_node);
-        return NULL;
-    }
-
-    /* If list is empty, set prev and next pointers of the node to itsef. */
-    if (!*list)
-    {
-        new_node->next = new_node;
-        new_node->prev = new_node;
-        *list = new_node;
-    }
-
-    /* Else, if list is not empty, set the pointers to insert it at the beginning */
-    else
-    {
-        tail = (*list)->prev;
-        new_node->next = *list;
-        new_node->prev = tail;
-        tail->next = new_node;
-        (*list)->prev = new_node;
-        *list = new_node;
-    }
-    return (new_node);
+	/* If list is empty, set prev and next pointers of the node to itself */
+	if (!*list)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else /* List is not empty, insert at the beginning */
+	{
+		tail = (*list)->prev;
+		new_node->next = *list;
+		new_node->prev = tail;
+		tail->next = new_node;
+		(*list)->prev = new_node;
+		*list = new_node;
+	}
+	return (new_node);
 }

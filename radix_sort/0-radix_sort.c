65406b469c134f94cdcdcdb4cc4fb1087bfce2fa
CamilleFavriel <camillefvrl@gmail.com>
2024-11-08 12:02:07 +0100

    Added Counting Sort (missing sorting in order)

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -1,6 +1,49 @@
 #include "sort.h"
 
 /**
+ * counting_sort_by_digit - 
+ * @array:
+ * @size:
+ * @int:
+ * 
+ * Return: 
+ */
+
+void counting_sort_by_digit(int *array, size_t size, int digit_place)
+{
+    int count[10] = {0};
+    int sorted[size];
+
+    /* Calculate count of elements */
+    for (int i = 0; i < size; i++)
+    {
+        int index = (array[i] / digit_place) % 10;
+        count[index]++;
+    }
+
+    /* Calculate cumulative count */
+    for (int i = 1; i < 10; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    /* Place the elements in sorted order */
+    for (int i = size - 1; i >= 0; i--)
+    {
+        /* Find the current digit based on diigt_place */
+        int current_digit = (array[i] / digit_place) % 10;
+    }
+
+    /* Copy the sorted elements into original array */
+    for (int i = 0; i < size; i++) {
+        array[i] = sorted[i];
+    }
+}
+
+
+
+
+/**
  * radic_sort - sorts an array of integers in ascending order using the Radix sort algorithm
  * @array: array that contain only nymbers >=0
  * 
@@ -9,5 +52,5 @@
 
 void radix_sort(int *array, size_t size)
 {
-    
+
 }

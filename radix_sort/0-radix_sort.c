14ebd804d8d964afd1776166838ad8723cfcf4d4
CamilleFavriel <camillefvrl@gmail.com>
2024-11-08 10:56:32 +0100

    Initialized Radix Sort Project

---
+++ radix_sort/0-radix_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * radic_sort - sorts an array of integers in ascending order using the Radix sort algorithm
+ * @array: array that contain only nymbers >=0
+ * 
+ * Return: Print the array each time you increase your significant digit
+ */
+
+void radix_sort(int *array, size_t size)
+{
+    
+}

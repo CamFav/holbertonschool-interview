60138aadb26a7e77e9f7145aec99adb10d3e0cab
CamilleFavriel <camillefvrl@gmail.com>
2024-11-08 20:49:07 +0100

    Betty coding style + functional counting sort and radix sort (LSD) working

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -10,41 +10,42 @@
  */
 void counting_sort_by_digit(int *array, size_t size, int digit_place)
 {
-    int count[10] = {0};
-    int sorted[size];
-    size_t i;
+	int count[10] = {0};
+	int sorted[size];
+	size_t i;
 
-    /* Calculate count of elements */
-    for (i = 0; i < size; i++)
-    {
-        int index = (array[i] / digit_place) % 10;
-        count[index]++;
-    }
+	/* Calculate count of elements */
+	for (i = 0; i < size; i++)
+	{
+		int index = (array[i] / digit_place) % 10;
+		count[index]++;
+	}
 
-    /* Calculate cumulative count */
-    for (i = 1; i < 10; i++)
-    {
-        count[i] += count[i - 1];
-    }
+	/* Calculate cumulative count */
+	for (i = 1; i < 10; i++)
+	{
+		count[i] += count[i - 1];
+	}
 
-    /* Place the elements in sorted ordSer */
-    for (i = size; i > 0; i--)
-    {
-        /* Find the current digit based on digit_place */
-        int current_digit = (array[i - 1] / digit_place) % 10;
-        sorted[count[current_digit] - 1] = array[i - 1];
-        count[current_digit]--;
-    }
+	/* Place the elements in sorted order */
+	for (i = size; i > 0; i--)
+	{
+		/* Find the current digit based on digit_place */
+		int current_digit = (array[i - 1] / digit_place) % 10;
+		sorted[count[current_digit] - 1] = array[i - 1];
+		count[current_digit]--;
+	}
 
-    /* Copy the sorted elements into the original array */
-    for (i = 0; i < size; i++)
-    {
-        array[i] = sorted[i];
-    }
+	/* Copy the sorted elements into the original array */
+	for (i = 0; i < size; i++)
+	{
+		array[i] = sorted[i];
+	}
 }
 
 /**
- * radix_sort - sorts an array of integers in ascending order using the Radix sort algorithm
+ * radix_sort - sorts an array of integers in ascending order using the Radix
+ *              sort algorithm
  * @array: array that contain only numbers >= 0
  * @size: The size of the array
  *
@@ -52,34 +53,34 @@ void counting_sort_by_digit(int *array, size_t size, int digit_place)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max_num;
-    size_t i;
-    int digit_place;
+	int max_num;
+	size_t i;
+	int digit_place;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    max_num = array[0];
+	max_num = array[0];
 
-    /* Find the maximum number to determine the number of digits */
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max_num)
-            max_num = array[i];
-    }
+	/* Find the maximum number to determine the number of digits */
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max_num)
+			max_num = array[i];
+	}
 
-    /* Sort by each digit place */
-    for (digit_place = 1; max_num / digit_place > 0; digit_place *= 10)
-    {
-        counting_sort_by_digit(array, size, digit_place);
+	/* Sort by each digit place */
+	for (digit_place = 1; max_num / digit_place > 0; digit_place *= 10)
+	{
+		counting_sort_by_digit(array, size, digit_place);
 
-        /* Print the array after each digit place sort */
-        for (i = 0; i < size; i++)
-        {
-            printf("%d", array[i]);
-            if (i < size - 1)
-                printf(", ");
-        }
-        printf("\n");
-    }
+		/* Print the array after each digit place sort */
+		for (i = 0; i < size; i++)
+		{
+			printf("%d", array[i]);
+			if (i < size - 1)
+				printf(", ");
+		}
+		printf("\n");
+	}
 }

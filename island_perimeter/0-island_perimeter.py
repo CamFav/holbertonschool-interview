bf897f1d436866e8af135cdbe43400937844efce
CamilleFavriel <camillefvrl@gmail.com>
2024-11-15 11:00:33 +0100

    Island Perimeter Algo

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the amount of rainwater trapped between walls.
+
+    Args:
+        grid (list of int): A list of integers representing an island.
+    """
+    # Perimeter of the island
+    perimeter = 0
+    
+    # Iterate over the grid
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            
+            # If the current cell is an island cell
+            if grid[i][j] == 1:
+                
+                # Check the top cell
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                    
+                # Check the bottom cell    
+                if i == len(grid)-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                    
+                # Check the left cell
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                    
+                # Check the right cell
+                if j == len(grid[0])-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+                    
+    return perimeter

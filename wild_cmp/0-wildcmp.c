7225f70cb05889b6901c4895d96fe3506889099a
CamilleFavriel <camillefvrl@gmail.com>
2024-11-29 16:24:11 +0100

    Fix: Betty Coding Style

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -10,22 +10,23 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    /* If both strings reach their end, return 1 because identical */
-    if (*s1 == '\0' && *s2 == '\0')
-    {
-        return (1);
-    }
+	/* If both strings reach their end, return 1 because identical */
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
 
-    if (*s2 == '*')
-    {
-        return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-    }
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	}
 
-    /* If characters match, continue comparing */
-    if (*s1 == *s2)
-    {
-        return (wildcmp(s1 + 1, s2 + 1));
-    }
-    /* Otherwise, return 0 because the strings aren't identical */
-    return (0);
+	/* If characters match, continue comparing */
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	/* Otherwise, return 0 because the strings aren't identical */
+	return (0);
 }

c01fcaacda743c3566dafd459dc7704ecf356eff
Camille F <camillefvrl@gmail.com>
2025-04-16 17:55:13 +0200

    Fixed error with compilation

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,10 +1,28 @@
 #include "lists.h"
 
-
+/**
+ * check_cycle - Checks if a singly linked list has a cycle
+ * @list: Pointer to the head of the list
+ * Return: 1 if cycle exists, 0 otherwise
+ */
 int check_cycle(listint_t *list)
 {
     listint_t *slow, *fast;
 
+    if (list == NULL || list->next == NULL)
+        return (0);
+
     slow = list;
-    fast = list;
+    fast = list->next;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        if (slow == fast)
+            return (1);
+        
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return (0);
 }
